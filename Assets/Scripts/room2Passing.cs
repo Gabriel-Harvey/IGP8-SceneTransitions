a28e74cc4f8a6e4bcb738d3d0b6e7a51aa5ba9a4
Gabriel-Harvey <gharve201@caledonian.ac.uk>
2021-03-23 12:55:38 +0000

    First Committ

---
+++ Assets/Scripts/room2Passing.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class room2Passing : MonoBehaviour
+{
+    //public Text textfromScene;
+
+    void Start()
+    {
+        //textfromScene.text = PlayerPrefs.GetString("lives");
+    }
+
+  
+}
---
+++ Assets/Scripts/sceenTransitions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceenTransitions : MonoBehaviour
+{
+    public UIManager manager;
+
+    public void Room2()
+    {
+        manager.livesTransfer();
+        SceneManager.LoadScene("Room 2");
+    }
+
+    
+}
